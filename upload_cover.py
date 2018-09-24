c7e7da964ebda09b351b499423dc44ca4f897e74
ifilatov <ivan@filatovz.ru>
2018-09-24 21:43:36 +0300

    Почистил код!

--- upload_cover.py
+++ upload_cover.py
@@ -4,9 +4,7 @@ import settings
 
 
 def main():
-    """ Пример загрузки фото """
 
-    # login, password = 'python@vk.com', 'mypassword'
     vk_session = vk_api.VkApi(settings.login, settings.password)
 
     try:
@@ -15,24 +13,17 @@ def main():
         print(error_msg)
         return
 
-    """ В VkUpload реализованы методы загрузки файлов в ВК
-    """
     vk = vk_session.get_api()
-    groups = vk.groups.get(
-        filter='editor',
-        extended=1
-    )
-    # group_id = groups['items'][1]
     upload = vk_api.VkUpload(vk_session)
     photo = upload.photo_cover(  # Подставьте свои данные
-        photo='./cover/cover.jpg',
+        photo=settings.cover_path,
         group_id=settings.group_id,
         crop_x=0,
         crop_y=0,
         crop_x2=1590,
         crop_y2=400
     )
-    print(photo)
+
 
 if __name__ == '__main__':
     main()
