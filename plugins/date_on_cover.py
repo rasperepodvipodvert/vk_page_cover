48b614e273722f34fb803db26d36a48c582919bf
ifilatov <ivan@filatovz.ru>
2018-09-25 00:14:02 +0300

    Плагин date_on_cover

--- plugins/date_on_cover.py
+++ plugins/date_on_cover.py
@@ -1,18 +1,19 @@
-import pathlib
-
 from PIL import Image, ImageDraw, ImageFont
 import settings
 import datetime
 import os
-
+from plugins.resize_and_crop import *
 
 def date_on_cover(
         font='./fonts/28days.ttf',
-        font_size=200,
+        font_size=150,
         text=datetime.datetime.strftime(datetime.datetime.now(), "%d.%m.%Y")
 
 ):
+    randome_file = getRandomFile(os.getcwd()+'/cover/')
     im = Image.open(settings.cover_path).convert('RGBA')
+    size = (1590, 400)
+    im = resize_and_crop(im, size, crop_type='top')
     txt = Image.new('RGBA', im.size, (255, 255, 255, 0))
     fnt = ImageFont.truetype(font, font_size)
     draw = ImageDraw.Draw(im)
@@ -22,7 +23,21 @@ def date_on_cover(
         fill=(255, 95, 51, 255),
         font=fnt
     )
+    draw.text(
+        text='by СИСADМИН',
+        xy=(720, 250),
+        fill=(255, 95, 51, 255),
+        font=fnt
+    )
     out = Image.alpha_composite(im, txt)
-    save_path = os.getcwd() + "/cover/date_on_cover.png"
+    save_path = os.getcwd() + "/date_on_cover.png"
     out.save(save_path, 'PNG')
     return save_path
+
+def getRandomFile(path):
+  """
+  Returns a random filename, chosen among the files of the given path.
+  """
+  files = os.listdir(path)
+  index = random.randrange(0, len(files))
+  return files[index]
--- upload_cover.py
+++ upload_cover.py
@@ -1,6 +1,5 @@
 # -*- coding: utf-8 -*-
 import vk_api
-import settings
 
 from plugins.date_on_cover import *  # подключаем плагины
 
@@ -15,16 +14,16 @@ def main():
         return
 
     upload = vk_api.VkUpload(vk_session)
-
-    photo = upload.photo_cover(  # Подставьте свои данные
-        photo=date_on_cover(),
-        group_id=settings.group_id,
-        crop_x=0,
-        crop_y=0,
-        crop_x2=1590,
-        crop_y2=400
-    )
-    return photo
+    date_on_cover()
+    # photo = upload.photo_cover(  # Подставьте свои данные
+    #     photo=date_on_cover(),
+    #     group_id=settings.group_id,
+    #     crop_x=0,
+    #     crop_y=0,
+    #     crop_x2=1590,
+    #     crop_y2=400
+    # )
+    # return photo
 
 
 if __name__ == '__main__':
